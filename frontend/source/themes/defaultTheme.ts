12f45bcf2b0cbf4073d88ea1b2bd84f6748b0a5c
Thomas <nguyen3lihn@gmail.com>
2026-07-22 13:31:54 +0200

    add theme

---
+++ frontend/source/themes/defaultTheme.ts
@@ -0,0 +1,65 @@
+import { createTheme } from "@mui/material";
+
+export const defaultTheme = createTheme({
+  components: {
+    MuiTextField: {
+      defaultProps: {
+        variant: "outlined"
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          color: "#fff",
+
+          "& .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#fff"
+          },
+
+          "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#FFD600"
+          },
+
+          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#FFD600"
+          }
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          color: "#fff",
+
+          "&.Mui-focused": {
+            color: "#FFD600"
+          }
+        }
+      }
+    },
+    MuiAlert: {
+      variants: [
+        {
+          props: { severity: "error" },
+          style: {
+            backgroundColor: "#f53a10",
+            color: "#000",
+
+            "& .MuiAlert-icon": {
+              color: "#000"
+            }
+          }
+        }
+      ]
+    }
+  },
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#000000"
+    },
+    secondary: {
+      main: "#ffed00"
+    }
+  }
+});
