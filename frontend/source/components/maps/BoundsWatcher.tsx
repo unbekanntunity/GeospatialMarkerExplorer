36afcf73ec30377f5b6757ed1d7a0e9fc41ca36e
Thomas <nguyen3lihn@gmail.com>
2026-07-31 00:49:53 +0200

    refactoring

--- frontend/source/components/maps/BoundsWatcher.tsx
+++ frontend/source/components/maps/BoundsWatcher.tsx
@@ -1,11 +1,13 @@
 import { LatLngBounds } from "leaflet";
 import { useMapEvents } from "react-leaflet";
 
-type BoundsWatcherProps = {
+interface IBoundsWatcherProps {
   onBoundsChange: (bounds: LatLngBounds) => void;
-};
+}
 
-const BoundsWatcher = ({ onBoundsChange }: BoundsWatcherProps) => {
+const BoundsWatcher = (props: IBoundsWatcherProps) => {
+  const { onBoundsChange } = props;
+
   const map = useMapEvents({
     moveend: () => {
       onBoundsChange(map.getBounds());
